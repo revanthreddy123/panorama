f971e79519822ea0325689dc98060d2434d3b005
revanthreddy123 <113545611+revanthreddy123@users.noreply.github.com>
2022-10-16 02:39:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const panoramaImage = new PANOLENS.ImagePanorama("images/pano.jpg");
+const panoramaImage = new PANOLENS.ImagePanorama("pano.jpg");
 const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({
