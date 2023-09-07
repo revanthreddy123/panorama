e38e8c2d5038f832c21e5b56991450012a9386ca
revanthreddy123 <ar.revanthreddy@gmail.com>
2023-09-07 15:19:36 +0530

    neoframes

--- main.js
+++
@@ -1,8 +0,0 @@
-const panoramaImage = new PANOLENS.ImagePanorama("pano.jpg");
-const imageContainer = document.querySelector(".image-container");
-
-const viewer = new PANOLENS.Viewer({
-  container: imageContainer,
-});
-
-viewer.add(panoramaImage);
